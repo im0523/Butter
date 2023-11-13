aaa7d70dc217f63e41f3917fca384dbbaa9911c7
im0523 <im0523@github.com>
2023-11-13 14:18:47 +0900

    Component test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+class Mycomponent1 extends Component {
+  render(){
+    return(
+      <div>
+        [미리 만든 빌딩 한 층]
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mycomponent1></Mycomponent1>
+      <Mycomponent1></Mycomponent1>
+      <Mycomponent1></Mycomponent1>
+      <Mycomponent1></Mycomponent1>
+      <Mycomponent1></Mycomponent1>
+      <Mycomponent1></Mycomponent1>
+      <Mycomponent1></Mycomponent1>
     </div>
   );
 }
